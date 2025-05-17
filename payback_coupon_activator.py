f2cad60f7a70646f83ac207c14793ee88e71f8bd
Robert Lasch <robert.lasch0@googlemail.com>
2025-05-17 17:12:15 +0200

    Fixes for Jetson Nano and latest Payback version

--- payback_coupon_activator.py
+++ payback_coupon_activator.py
@@ -1,12 +1,11 @@
 from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import os
 import platform
-
 
 def element(by, value, parent, timeout=10):
     return WebDriverWait(parent, timeout).until(
@@ -15,19 +14,14 @@ def element(by, value, parent, timeout=10):
 
 
 def main():
-    if platform.system() == "Windows":
-        user_data_dir = "~\\AppData\\Local\\Google\\Chrome\\User Data"
-
-    elif platform.system() == "Linux":
-        user_data_dir = "~/.config/google-chrome"
-
-    elif platform.system() == "Darwin":
-        user_data_dir = "~/Library/Application Support/Google/Chrome"
+    if platform.system() == "Linux":
+        user_data_dir = "~/.mozilla/firefox/5qds3ttw.default-release/"
 
     options = Options()
-    options.add_argument("user-data-dir=" + os.path.expanduser(user_data_dir))
+    options.add_argument("-headless")
+    options.add_argument("-profile=" + os.path.expanduser(user_data_dir))
 
-    service = Service()
+    service = Service("/usr/bin/geckodriver")
     driver = Chrome(options, service)
     driver.get("https://www.payback.de/coupons")
     center = element(By.ID, "coupon-center", driver).shadow_root
@@ -37,11 +31,16 @@ def main():
     container = element(
         By.CLASS_NAME, "coupon-center__container-published-coupons", center
     )
+    # Wait for coupons to load
+    WebDriverWait(container, 10).until(expected_conditions.presence_of_element_located((By.TAG_NAME, "pbc-coupon")))
     coupons = container.find_elements(By.TAG_NAME, "pbc-coupon")
 
     for index, coupon in enumerate(coupons):
         button = element(
             By.CSS_SELECTOR, "pbc-coupon-call-to-action", coupon.shadow_root
+        )
+        button = element(
+            By.CSS_SELECTOR, "button", button.shadow_root
         )
         button.click()
         print("Coupon", index + 1, "/", len(coupons), "activated")
